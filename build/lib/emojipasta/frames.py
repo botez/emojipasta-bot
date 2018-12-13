31c7bc4d4266bded65831dfb2dc4ed408a0ba555
Troy Olson <troyolson1@gmail.com>
2018-12-13 14:53:13 -0800

    fixed error in frames.py
    can't use "911" and "9112" as variable names(?)

--- build/lib/emojipasta/frames.py
+++ build/lib/emojipasta/frames.py
@@ -351,7 +351,13 @@ class Frames():
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command(description="frame")
-    async def 911(self):
+    async def x911(self):
+        pass
+
+    
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    @commands.command(description="frame")
+    async def x9112(self):
         pass
 
     @commands.cooldown(1, 5, commands.BucketType.user)
